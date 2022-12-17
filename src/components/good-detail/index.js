9b5a1534edd8a96c20e6dd448a8ef9e0e495055b
alex <tte577es7@yandex.ru>
2022-12-17 13:58:08 +0300

    less 41

--- src/components/good-detail/index.js
+++ src/components/good-detail/index.js
@@ -4,20 +4,46 @@ import './index.css'
 
 import goodsJSON from '../../stub/goods.json'
 
+const formSaveRef = React.createRef();
+
+
+//TODO Переделаем компонент на функциональный
 /**
  * GoodList - Отдельная стараница с инормацией о товаре и возможностью ее редактировать
  */
 class GoodDetailt extends React.Component{
     constructor(){
         super()
+        this.state = {
+            goods: goodsJSON,
+        }
+    }
+    saveGood(event){
+        console.log('Метод сохранения новых данных о товаре')
+        event.preventDefault()
+        console.log(event)
+
+        //Используем форм дату, для получения и отправки формы на сервер , во всех наших формах
+        //Получаем из рефа нативную(элемент из чистого JS) форму
+        const nativeForm = formSaveRef.current
+        //Передае форм дате нативную форму
+        const formData = new FormData(nativeForm)
+
+        //Так как в нашем апи ОТДЕЛЬНЫЕ роуты для работы с файлами, первы делом, мы ОТДЕЛЬНО получаем и отправляем фафл на сервер
+        // Получим файл
+        const file = formData.get('IMG')
+        //TODO Отправить файлы с изоюбражением для сохранения, а дальше сохраняем оставшиеся данные 
+
+        //Отправляем форм дату на сервер
     }
     render(){
         //Костыль для полуячения ID из адресной строки, для дальнейшего отображения товара
         const idGood = window.location.pathname.replace( '/goods/', '')
         // найти данные о конкретном товаре по id
-        const good = goodsJSON.find( good => good.ID == idGood)
+        const good = this.state.goods.find( good => good.ID == idGood)
         // ДЗ 1
         // Починить инпуты для редактирования
+        // Самый правильный и простой вариант, который был подсказан в консоли реактом - defaultValue
 
         // ДЗ 2 (усложненное)
         // Реализовать редактирование товара (!!без картинки!!)
@@ -26,17 +52,22 @@ class GoodDetailt extends React.Component{
         // - возвращаемся на главную страницу и видим отредактированный товар 
 
         //ВСПОМНИТЬ про жизненный цикл VUE
+
+        //Используем дефолтный value, чтобы дальше его без труда менять
+        //https://reactjs.org/docs/uncontrolled-components.html
+
+        // Пример с отправкой файла с формы - https://refine.dev/blog/how-to-multipart-upload/
         return(
             <>
                 <h1>{good.TITLE}</h1>
                 <img className='detail-img' src={good.IMG}/>
-                <form>
-                    <p>Название товара: <input type='text' name='TITLE' value={good.TITLE}/></p>
-                    <p>Описание товара: <input type='text' name='DISCR' value={good.DISCR}/></p>
-                    <p>Цена товара: <input type='text' name='PRICE' value={good.PRICE}/></p>
-                    <p>Количество товара: <input type='text' value={good.PRICE}/></p>
+                <form ref={formSaveRef} encType="multipart/form-data">
+                    <p>Название товара: <input type='text' name='TITLE' defaultValue={good.TITLE}/></p>
+                    <p>Описание товара: <input type='text' name='DISCR' defaultValue={good.DISCR}/></p>
+                    <p>Цена товара: <input type='text' name='PRICE' defaultValue={good.PRICE}/></p>
+                    <p>Количество товара: <input type='text' defaultValue={good.PRICE}/></p>
                     <p>Изображение товара: <input type='file' name="IMG"/></p>
-                    <p><input type='submit' value='Сохранить'/></p>
+                    <p><input type='submit' onClick={(event) => this.saveGood(event)} value='Сохранить'/></p>
                 </form>
             </>
         )
--- src/components/good-list/index.js
+++ src/components/good-list/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
 
 import './index.css'
 import GoodItem from '../good-item'
+import {Loader} from '../loader'
 
 import goodsJSON from '../../stub/goods.json'
 //Создали реф для получения данных введенные в поле поиска
@@ -14,9 +16,14 @@ class GoodList extends React.Component{
     constructor(){
         super()
         this.state = {
-            goods: goodsJSON,
+            goods: [],
             filteredGoods: null,
+            isLoading: true,
         }
+
+        // Метод bind жестко закрепляет контекст внутри метода, который мы в bind передаем
+        // Жестко закрепили контекст в методе
+        // this.delGood = this.delGood.bind(this);
     }
     findGood(event){
         //Получаем введенное в инпет значение, через реф
@@ -38,28 +45,82 @@ class GoodList extends React.Component{
         }
 
     }
-    delGood(id, context){
+    //Превратив метод в стрелочную функцию, мы смогли сохранить контекст, даже пробросивм метод в другой компонент
+    delGood = (id, context) => {
+
         console.log(`Удаляем товар ${id}`)
         console.log(`context`, context)
-        const goods = context.state.filteredGoods || context.state.goods
+        console.log(`this`, this)
+        console.log(`hello`, hello)
+
+        const goods = this.state.filteredGoods || this.state.goods
         const newFilteredGoods = goods.filter((good) =>
             good.ID !== id
         )
 
-        context.setState({
+        this.setState({
             goods: newFilteredGoods,
             filteredGoods: newFilteredGoods
         })
     }
+
+    _delComponent(){
+        console.log('Удаляем компонент GoodList')
+        //Приверить актуальное удаление товаров
+        ReactDOM.unmount(
+            document.getElementById("root")
+        ) 
+    }
+    // Является устаревший, но до сих пор есть в классовых компонентах реакта
+    componentWillMount() {
+        console.log("componentWillMount метод работающий до этапа монтирования")
+    }
+    componentDidMount() {
+        console.log("componentDidMount метод работающий после  этапа монтирования")
+        // тут мы будем запрашивать данные из АПИ и записывать в состояние
+        //Метод для реализации ожидания, перед отработкой алгоритма
+        //https://learn.javascript.ru/settimeout-setinterval
+        //Эмитация загрузки
+        // ДЗ - сделать отдельный(красивый), компонент для загрузки
+        setTimeout(() => {
+            this.setState({
+                goods: goodsJSON,
+                isLoading: false
+            })
+        }, 1000);
+    }
+    // Устаревший
+    componentWillUpdate() {
+        console.log("componentWillUpdat срабатывает после обновлении компонента")
+    }
+    componentDidUpdate() {
+        console.log("componentDidUpdate срабатывает перед обновлением компонента")
+    }
+    //Хук для переххвата ошибок, не работает с новым роутером
+    /*componentDidCatch(error, errorInfo) {
+        console.log('componentDidCatch')
+        console.log('error', error)
+        console.log('errorInfo', errorInfo)
+    }*/
     render(){
         //Ищем товары сначало в отфильтрованных, если их там нет, то в обычном блоке
         const goods = this.state.filteredGoods || this.state.goods
+        if (!goods) {
+            throw new Error('Не удалось, получить товары от сервера');
+        }
+
+        //Вывод лоадера, во время загрузки компонента
+        if(this.state.isLoading){
+            return <Loader />
+        }
+
         return(
             <React.Fragment>
                 <div>
                     ПОИСК
                     <input ref={inputSearchRef} type='text'/> 
                     <input type='submit' onClick={(event) => {this.findGood(event)}} value='Найти'/>
+                    <button onClick={this._delComponent.bind(this)} >Удалить компонент GoodList</button>
                 </div>
                 <div className='card-list'>
                 {      
